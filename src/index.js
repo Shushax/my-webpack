99bb0d90ba31274ece968c58a6d9fcc74d5d35df
Ksenia Sidorova <nekfi@mail.ru>
2020-09-11 11:08:37 +0300

    Правки

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import './css/style.css';
 
 import './js/app';
 
+import './js/2__homework/app';
+
+import './js/2__homework/domain';
+
+import './js/2__homework/game';
+
 // TODO: write your code in app.js
